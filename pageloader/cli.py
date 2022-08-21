bd93848913d76a8575ecdd193382cb91609e5372
tork <yuriy.kormin@gmail.com>
2022-08-22 00:53:16 +0400

    add parser args - output and address

--- pageloader/cli.py
+++ pageloader/cli.py
@@ -4,7 +4,18 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser(
         prog='page-loader',
-        description='hello'
+        description='page downloader'
+    )
+    parser.add_argument (
+        '--output',
+        type = str,
+        help = 'set destination EXISTING folder',
+        default = None
+    )
+    parser.add_argument (
+        'address',
+        type = str,
+        help = 'resource address to download'
     )
     args = parser.parse_args()
     return args
--- pageloader/scripts/pageloader.py
+++ pageloader/scripts/pageloader.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 from pageloader.cli import parse_args
+from pageloader.download import download
 
 
 def main():
-    parse_args()
+    args = parse_args()
+    path = download(args.address, args.output)
+    print (path)
 
 
 if __name__ == '__main__':
