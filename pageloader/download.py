e2f321de970ac34f54fdb4c5b4d2ccebe255d642
tork <yuriy.kormin@gmail.com>
2022-08-22 10:02:26 +0400

    download selected resource

--- pageloader/download.py
+++ pageloader/download.py
@@ -1,3 +1,5 @@
+import re
+
 import os
 import requests
 
@@ -6,4 +8,17 @@ def download(address, path=None):
     if path is None:
         path = os.getcwd()
     request = requests.get(address)
-    return request.text
+    path = os.path.join(path, render_filename(address))
+    save_to_file(request.text, path)
+    return path
+
+
+def render_filename(address):
+    filename = re.sub(r'(^https?:\/\/|\.[a-zA-Z]+$)', '', address)
+    filename = re.sub(r'[^0-9a-zA-Z]', '-', filename)
+    return f'{filename}.html'
+
+
+def save_to_file(data, path):
+    with open(path, "w") as f:
+        f.write(data)
