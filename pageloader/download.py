ba955ae8266f3db519f48ee60f67c7db65dda1e4
tork <yuriy.kormin@gmail.com>
2022-08-23 12:42:32 +0400

    add bs4 to poetry, rename render_html def

--- pageloader/download.py
+++ pageloader/download.py
@@ -1,5 +1,4 @@
 import re
-
 import os
 import requests
 
@@ -8,12 +7,12 @@ def download(address, path=None):
     if path is None:
         path = os.getcwd()
     request = requests.get(address)
-    path = os.path.join(path, render_filename(address))
+    path = os.path.join(path, render_html_filename(address))
     save_to_file(request.text, path)
     return path
 
 
-def render_filename(address):
+def render_html_filename(address):
     filename = re.sub(r'(^https?:\/\/|\.[a-zA-Z]+$)', '', address)
     filename = re.sub(r'[^\da-zA-Z]', '-', filename)
     return f'{filename}.html'
