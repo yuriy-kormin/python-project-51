0b669e6567bc26c723417eedfc8a5431dff9feff
tork <yuriy.kormin@gmail.com>
2022-08-26 00:15:30 +0400

    fix tests html_file_exists, html content correct(without links modifying

--- tests/test_output_file_content.py
+++ tests/test_output_file_content.py
@@ -1,13 +1,12 @@
-import os
 import tempfile
 from pageloader import download
 
 
-def test_result_html(test_url, test_filename, simple_html_content, requests_mock):
-    req_mock = requests_mock.get(test_url, text=simple_html_content)
+def test_result_html(test_url, test_filename,
+                     simple_html_content, requests_mock):
+    requests_mock.get(test_url, text=simple_html_content)
     with tempfile.TemporaryDirectory() as tmpdir:
         result_path = download(test_url, tmpdir)
-        result_html_path = os.path.join(result_path, test_filename)
         with open(result_path, 'r') as f:
             result_html_content = f.read()
     assert result_html_content == simple_html_content
--- tests/test_output_names.py
+++ tests/test_output_names.py
@@ -1,15 +1,14 @@
 import os
 import tempfile
-import pook
 from pageloader import download
 
-#
-# @pook.on
-# def test_result_file(test_url, test_filename, test_subdir_name):
-#     test_url_ext = f'{test_url}.mht'
-#     pook.get(test_url_ext)
-#     with tempfile.TemporaryDirectory() as tmpdirname:
-#         test_file = os.path.join(tmpdirname, test_filename)
-#         assert download(test_url_ext, tmpdirname) == test_file
-#         assert os.path.exists(test_file)
-#         assert os.path.exists(os.path.join(tmpdirname, test_subdir_name))
+
+def test_name_html_file(test_url, test_filename,
+                        test_subdir_name, requests_mock):
+    test_url_ext = f'{test_url}.mht'
+    requests_mock.get(test_url_ext)
+    with tempfile.TemporaryDirectory() as tmpdir:
+        test_file = os.path.join(tmpdir, test_filename)
+        assert download(test_url_ext, tmpdir) == test_file
+        assert os.path.exists(test_file)
+        assert os.path.exists(os.path.join(tmpdir, test_subdir_name))
