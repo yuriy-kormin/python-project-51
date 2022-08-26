253337b98c921cc87edd682611c33276ae8f1533
tork <yuriy.kormin@gmail.com>
2022-08-26 10:53:22 +0400

    add tests for checking downloaded image content

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,6 +29,17 @@ def simple_html_content(fixtures_path):
 
 
 @pytest.fixture
+def html_content_with_img_link(fixtures_path):
+    html_path = os.path.join(fixtures_path, 'with_image_link.html')
+    return read_file(html_path)
+
+
+@pytest.fixture
+def image_path(fixtures_path):
+    return os.path.join(fixtures_path, 'python.png')
+
+
+@pytest.fixture
 def fixtures_path():
     return os.path.join(os.path.dirname(__file__), FIXTURES_PATH)
 
--- tests/test_download_files_correct.py
+++ tests/test_download_files_correct.py
@@ -1,22 +1,18 @@
 import os
 import tempfile
 from pageloader import download
-from pageloader.download import render_name
-from PIL import Image
-
-#
-# def test_corrected_downloading(test_url, test_filename):
-#     with tempfile.TemporaryDirectory() as tmpdirname:
-#         download(test_url, tmpdirname)
-#         subdir_name = render_name(test_url, 'subdir')
-#         subdir_path = os.path.join(tmpdirname, subdir_name)
-#         flag = True
-#         for file in os.listdir(subdir_path):
-#             try:
-#                 file_path = os.path.join(subdir_path,file)
-#                 image = Image.open(file_path).verify()
-#             except:
-#                 flag = False
-#         assert flag == True
+import filetype
 
 
+def test_corrected_downloading(test_url, test_filename, requests_mock, image_path,
+                               html_content_with_img_link, test_subdir_name):
+    with tempfile.TemporaryDirectory() as tmpdir, open(image_path, 'rb') as f:
+        requests_mock.get(test_url, text=html_content_with_img_link)
+        requests_mock.get('https://hexlet.io/python.png', content=f.read())
+        download(test_url, tmpdir)
+        subdir_path = os.path.join(tmpdir, test_subdir_name, 'hexlet-io-python.png')
+        with open(subdir_path, 'rb') as download_image, open(image_path,'rb') as source_image:
+            download_image_data = download_image.read()
+            source_image_data = source_image.read()
+        assert filetype.is_image(subdir_path)
+    assert download_image_data == source_image_data
