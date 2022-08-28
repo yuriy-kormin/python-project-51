11cb056d753e43ab2a4bdb5e3f1282a09379a3f2
tork <yuriy.kormin@gmail.com>
2022-08-28 23:40:17 +0400

    modify need_to_download. def make dicision to download a file and return full link path doesnot matter, link relative or not and return None if decision of downloading is negative

--- pageloader/download.py
+++ pageloader/download.py
@@ -1,8 +1,8 @@
 import re
 import os
 import requests
-from urllib.parse import urlparse
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse, urljoin
 
 
 def download(address, path=None):
@@ -16,48 +16,6 @@ def download(address, path=None):
     return file_path
 
 
-def parse_html(src_address, file_path, subdir_name):
-    result = ''
-    with open(file_path, 'r+') as f:
-        file_data = f.read()
-        soup = BeautifulSoup(file_data, 'html.parser')
-        process_img_src(soup, src_address, subdir_name)
-        process_other_src(soup, src_address, subdir_name)
-        f.seek(0)
-        f.write(soup.prettify())
-    return result
-
-
-def process_other_src(soup, src_address, subdir):
-    for tag_name in ('link', 'script'):
-        tags = soup.findAll(tag_name, {'href': True})
-        for tag in tags:
-            if need_to_download(src_address, tag['href']):
-                tag['href'] = download_file(tag['href'], subdir)
-
-
-def process_img_src(soup, src_address, subdir):
-    tags = soup.findAll('img')
-    for tag in tags:
-        if need_to_download(src_address, tag['src']):
-            tag['src'] = download_file(tag['src'], subdir)
-
-
-def need_to_download(src_address, obj_href):
-    source_url, img_url = map(url_parse, (src_address, obj_href))
-    print(obj_href)
-    _, ext = os.path.splitext(img_url['full_path'])
-    if img_url['loc'] == source_url['loc'] and len(ext) > 0:
-        return True
-
-
-def download_file(address, subdir_name):
-    file_path = os.path.join(subdir_name, render_name(address, 'file_name'))
-    request = requests.get(address, stream=True)
-    save_to_file(request.content, file_path, binary=True)
-    return file_path
-
-
 def make_subdir(address, path):
     name = render_name(address, 'subdir')
     subdir_path = os.path.join(path, name)
@@ -65,16 +23,18 @@ def make_subdir(address, path):
     return subdir_path
 
 
-def render_name(address, output_type):
-    url = url_parse(address)
+def render_name(url, output_type):
+    url_data = url_parse(url)
     if output_type == 'html':
-        return f"{url['loc']}{url['path']}.html"
+        return f"{url_data['loc']}{url_data['path']}.html"
     elif output_type == 'subdir':
-        return f"{url['loc']}_files"
-    elif output_type == 'file_name':
-        name, ext = os.path.splitext(url['full_path'])
+        return f"{url_data['loc']}_files"
+    elif output_type == 'file':
+        name, ext = os.path.splitext(url_data['full_path'])
         name = replace_symbols(name)
-        return f"{url['loc']}{name}{ext}"
+        if not ext:
+            ext = '.html'
+        return f"{url_data['loc']}{name}{ext}"
 
 
 def replace_symbols(data):
@@ -93,3 +53,53 @@ def save_to_file(data, path, binary=False):
     mode = 'wb' if binary else 'w'
     with open(path, mode) as f:
         f.write(data)
+
+
+def parse_html(address, file_path, subdir_name):
+    result = ''
+    with open(file_path, 'r+') as f:
+        file_data = f.read()
+        soup = BeautifulSoup(file_data, 'html.parser')
+        process_img_src(soup, address, subdir_name)
+        process_other_src(soup, address, subdir_name)
+        f.seek(0)
+        f.write(soup.prettify())
+    return result
+
+
+def process_other_src(soup, address, subdir):
+    for tag_name in ('link', 'script'):
+        tags = soup.findAll(tag_name, {'href': True})
+        for tag in tags:
+            print(tag['href'])
+            full_url = need_to_download(address, tag['href'])
+            if full_url:
+                print('downloading...')
+                tag['href'] = download_file(full_url, subdir)
+
+
+def process_img_src(soup, address, subdir):
+    tags = soup.findAll('img')
+    for tag in tags:
+        url = need_to_download(address, tag['src'])
+        if url:
+            tag['src'] = download_file(url, subdir)
+
+
+def need_to_download(address: str, obj_href: str) -> str:
+    ''' Check that address and  href on the same domain name and
+    must be downloaded. return link to download file or None '''
+    source_url, obj_url = map(url_parse, (address, obj_href))
+    if not obj_url['loc']:
+        return urljoin(address, obj_href)
+    _, ext = os.path.splitext(obj_url['full_path'])
+    if obj_url['loc'] == source_url['loc']:
+        return obj_href
+    return
+
+
+def download_file(url, subdir_name):
+    file_path = os.path.join(subdir_name, render_name(url, 'file'))
+    request = requests.get(url, stream=True)
+    save_to_file(request.content, file_path, binary=True)
+    return file_path
