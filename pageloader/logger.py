f4955f317d7156d9820397a67810841309967d2b
tork <yuriy.kormin@gmail.com>
2022-09-04 13:43:14 +0400

    add stderr handler for logger

--- pageloader/logger.py
+++ pageloader/logger.py
@@ -1,10 +1,16 @@
 import logging
+import sys
 LOGNAME = 'page_loader.log'
 
 
 def setup_logger():
     logging.basicConfig(filename=LOGNAME, filemode='a',
                         encoding='utf-8', level=logging.DEBUG)
-    console = logging.StreamHandler()
-    console.setLevel(logging.INFO)
-    logging.getLogger('').addHandler(console)
+    stdout_handler = logging.StreamHandler(stream=sys.stdout)
+    stdout_handler.setLevel(logging.INFO)
+    stderr_handler = logging.StreamHandler(stream=sys.stderr)
+    stderr_handler.setLevel(logging.ERROR)
+    stdout_handler.setLevel(logging.INFO)
+    logging.getLogger('').addHandler(stdout_handler)
+    logging.getLogger('').addHandler(stderr_handler)
+
