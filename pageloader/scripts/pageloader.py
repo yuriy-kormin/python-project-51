d338f94714035de5301e7fa4599134616697bac7
tork <yuriy.kormin@gmail.com>
2022-09-04 22:29:51 +0400

    try - except to  requests library errors

--- pageloader/scripts/pageloader.py
+++ pageloader/scripts/pageloader.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
+import requests
 from pageloader.cli import parse_args
 from pageloader import download
-import errno
 import logging
 
 
@@ -9,11 +9,20 @@ def main():
     args = parse_args()
     try:
         download(args.address, args.output)
-    except Exception as e:
-        if e.errno == errno.ENOENT:
-            logging.error('specified path does not exists')
-        elif e.errno == errno.EACCES:
-            logging.error('specified path write permission denied')
+    except FileNotFoundError:
+        logging.error('Specified path does not exists')
+    except PermissionError:
+        logging.error('Specified path write permission denied')
+    except requests.URLRequired:
+        logging.error('Invalid URL address')
+    except requests.ConnectionError:
+        logging.error(
+            'Cannot connect to specified URL. Probably, some network problem')
+    except requests.TooManyRedirects:
+        logging.error('Too many redirects on address')
+    except requests.Timeout:
+        logging.error(
+            'Request timed out while trying to connect to the remote server.')
 
 
 if __name__ == '__main__':
