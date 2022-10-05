e705c02f74dc2744351ed696dce1c7fc97276e0a
tork <yuriy.kormin@gmail.com>
2022-10-05 13:44:34 +0400

    make test for exceptions:     1) subdir exists or havenot write permissions

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -16,6 +16,27 @@ def test_write_output_path(url):
             download(url, path)
 
 
+def test_subdir_permissions(
+        url, subdir_name, output_html_name, html_with_links):
+    with tempfile.TemporaryDirectory() as tmpdir, \
+            requests_mock.Mocker() as mock:
+        mock.get(requests_mock.ANY, text='')
+        mock.get(url, text=html_with_links)
+        download_path = os.path.join(tmpdir, 'subdir')
+        os.mkdir(download_path)
+        path = os.path.join(download_path, output_html_name)
+        open(path, 'a').close()
+        os.chmod(download_path, 0o444)
+        with pytest.raises(PermissionError):
+            download(url, download_path)
+        os.chmod(download_path, 0o777)
+        subdir_path = os.path.join(download_path, subdir_name)
+        os.mkdir(subdir_path)
+        os.chmod(subdir_path, 0o000)
+        with pytest.raises(PermissionError):
+            download(url, download_path)
+
+
 def test_invalid_url(url):
     with requests_mock.Mocker() as mock:
         with pytest.raises(IOError):
